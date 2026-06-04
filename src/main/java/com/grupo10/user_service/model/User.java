1b4084a8609821014f9635b681ba3feeac0bc70d
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-03 21:58:57 -0400

    feat: se crea entidad para usuario para mapear tb_usuario

---
+++ src/main/java/com/grupo10/user_service/model/User.java
@@ -0,0 +1,43 @@
+package com.grupo10.user_service.model;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Data
+@Entity
+@Table(name = "tb_usuario")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private int id;
+    
+    @Column(name = "email")
+    private String correo;
+   
+    @Column(name = "password_hash")
+    private String password;
+    
+    @Column(name = "rol")
+    private String rol;
+
+    @Column(name = "estado")
+    private boolean estado;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime createdAt;
+
+
+
+}
