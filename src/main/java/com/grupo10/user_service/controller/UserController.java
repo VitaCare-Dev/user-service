249c23c54c421df899bd361bea21253cf6278fa1
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-04 00:55:45 -0400

    feat: se crea controller para registro de usuarios

---
+++ src/main/java/com/grupo10/user_service/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.grupo10.user_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.grupo10.user_service.service.UserService;
+import com.grupo10.user_service.dto.UserRequestDto;
+import com.grupo10.user_service.dto.UserResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto request) {
+        UserResponseDto createdUser = userService.createUser(request);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+
+
+}
